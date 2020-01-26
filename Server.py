b4623bf4fce121b047106913bcde82b0f2caad7d
Animark159 <thornemark58@gmail.com>
2020-01-26 17:16:06 +0000

    Fixed OS code.

--- Server.py
+++ Server.py
@@ -6,7 +6,7 @@ port = int(os.environ["PORT"])
 
 @ServerApp.route('/',methods=['GET'])
 def Hello():
-    print "Homepage loaded!"
+    print ("Homepage loaded!")
     return render_template("index.html")
 
 ServerApp.run(port=port,host="0.0.0.0")
